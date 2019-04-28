699c60b8cc17d05106803b74e4af1f24ed533954
Tom Stephens <thomas.stephens@gmail.com>
2019-04-28 08:24:46 -0600

    update loop to check that line matchs star system information
    This is in preparation to adding in jump route data and we need to be able to differentiate between the data records.

--- StarMapGen/src/loadData.py
+++ StarMapGen/src/loadData.py
@@ -15,35 +15,36 @@ def loadData(fName,param):
 
     sysList = []
     for line in f:
-        s=StarSystem(param) #note that this currently creates a random star system that we will overwrite
-        #@todo should probably make a default constructor
         #read in system name
         m = re.match('Name:\s*(.*)',line)
-        s.name = m.group(1)
-#        print("Name:",s.name)
-        #read in coordinates
-        line = f.readline()
-        p=re.compile(r'(\d+),(\d+),([-]*\d+)')
-        m=p.search(line)
-        s.x=int(m.group(1))
-        s.y=int(m.group(2))
-        s.z=int(m.group(3))
-        s.mapPos=(s.x,s.y)
-#        print ("Coordinates: ("+s.x+","+s.y+","+s.z+")")
-        # read in star count
-        line = f.readline()
-        m = re.match('Number of Stars: (\d+)',line)
-        s.nStars = int(m.group(1))
-#        print("Number of Stars:",s.nStars)
-        # read in spectral types
-        line = f.readline()
-        m = re.match('Spectral Types:\s*(.*)',line)
-        s.stars = m.group(1).split(", ")
-#        print ("Spectral Types:",", ".join(str(x) for x in s.stars))
-        #read blankline
-        line = f.readline()
-#        print (line,end="")
-        sysList.append(s)
+        if (m):
+            s=StarSystem(param) #note that this currently creates a random star system that we will overwrite
+            #@todo should probably make a default constructor
+            s.name = m.group(1)
+    #        print("Name:",s.name)
+            #read in coordinates
+            line = f.readline()
+            p=re.compile(r'(\d+),(\d+),([-]*\d+)')
+            m=p.search(line)
+            s.x=int(m.group(1))
+            s.y=int(m.group(2))
+            s.z=int(m.group(3))
+            s.mapPos=(s.x,s.y)
+    #        print ("Coordinates: ("+s.x+","+s.y+","+s.z+")")
+            # read in star count
+            line = f.readline()
+            m = re.match('Number of Stars: (\d+)',line)
+            s.nStars = int(m.group(1))
+    #        print("Number of Stars:",s.nStars)
+            # read in spectral types
+            line = f.readline()
+            m = re.match('Spectral Types:\s*(.*)',line)
+            s.stars = m.group(1).split(", ")
+    #        print ("Spectral Types:",", ".join(str(x) for x in s.stars))
+            #read blankline
+            line = f.readline()
+    #        print (line,end="")
+            sysList.append(s)
 
     f.close()
     return sysList
