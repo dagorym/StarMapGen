2cc27fdecfbf13d142ace676144e07b25af7aa6b
Tom Stephens <thomas.stephens@gmail.com>
2019-06-13 10:24:33 -0600

    Add code to randomly generate spectral subtypes (0-9)

--- StarMapGen/src/StarSystem.py
+++ StarMapGen/src/StarSystem.py
@@ -62,6 +62,68 @@ class StarSystem:
             else:
                 self.nStars = 10
     
+    def _getMSSubType(self,spClass):
+        """Calculates the spectral subtype (0-9) for main sequence stars.
+
+        This is based roughly on the Kroupa IMF function and could be refined
+        """
+        roll = randint(1,100)
+        set1 = ['A','F','G','K','M']
+        if spClass in set1:
+            if (9 > roll):
+                return spClass+"0"
+            elif (18 > roll):
+                return spClass+"1"
+            elif (27 > roll):
+                return spClass+"2"
+            elif (36 > roll):
+                return spClass+"3"
+            elif (46 > roll):
+                return spClass+"4"
+            elif (56 > roll):
+                return spClass+"5"
+            elif (67 > roll):
+                return spClass+"6"
+            elif (78 > roll):
+                return spClass+"7"
+            elif (89 > roll):
+                return spClass+"8"
+            else:
+                return spClass+"9"
+        if (spClass == "B"):
+            if (5 > roll):
+                return spClass+"0"
+            elif (11 > roll):
+                return spClass+"1"
+            elif (17 > roll):
+                return spClass+"2"
+            elif (22 > roll):
+                return spClass+"3"
+            elif (29 > roll):
+                return spClass+"4"
+            elif (38 > roll):
+                return spClass+"5"
+            elif (49 > roll):
+                return spClass+"6"
+            elif (63 > roll):
+                return spClass+"7"
+            elif (80 > roll):
+                return spClass+"8"
+            else:
+                return spClass+"9"
+        if (spClass == "O"):
+            if (13 > roll):
+                return "O5"
+            elif (27 > roll):
+                return "O6"
+            elif (43 > roll):
+                return "O7"
+            elif (61 > roll):
+                return "O8"
+            else:
+                return "O9"
+
+                
     def _generateSpectralType(self):
         """Determines the spectral type of each star in the system.
         
@@ -79,27 +141,27 @@ class StarSystem:
             if (9 > roll):
                 self.stars.append("BD")
             elif (83 > roll):
-                self.stars.append("M0")
+                self.stars.append(self._getMSSubType("M"))
             elif (90 > roll):
-                self.stars.append("K0")
+                self.stars.append(self._getMSSubType("K"))
             elif (93 > roll):
-                self.stars.append("G0")
+                self.stars.append(self._getMSSubType("G"))
             elif (95 > roll):
-                self.stars.append("F0")
+                self.stars.append(self._getMSSubType("F"))
             elif (100 > roll):
                 self.stars.append("WD")
             else:
                 r2 = randint(1,1000)
                 if (584 > r2):
-                    self.stars.append("A0")
+                    self.stars.append(self._getMSSubType("A"))
                 elif (667 > r2):
-                    self.stars.append("B0")
+                    self.stars.append(self._getMSSubType("B"))
                 elif (1000 > r2): #giants
                     self.stars.append(self._getGiantSpectralType()+"III")
                 else:
                     r4 = randint(1,100)
                     if (10 > r4):
-                        self.stars.append("O0")
+                        self.stars.append(self._getMSSubType("O"))
                     elif (100 > r4):
                         self.stars.append(self._getGiantSpectralType()+"I")
                     else:
@@ -119,19 +181,19 @@ class StarSystem:
         """
         roll = randint(1,100)
         if (83 > roll):
-            return "M0"
+            return "M%d" % randint(0,9)
         elif (90 > roll):
-            return "K0"
+            return "K%d" % randint(0,9)
         elif (93 > roll):
-            return "G0"
+            return "G%d" % randint(0,9)
         elif (95 > roll):
-            return "F0"
+            return "F%d" % randint(0,9)
         else:
-            return "K0"
+            return "K%d" % randint(0,9)
     
     def hasHabitable(self):
         for s in self.stars:
-            if ("F0" == s or "G0" == s or "K0" == s):
+            if ("F" == s[0:1] or "G" == s[0:1] or "K" == s[0:1]):
                 return True
         return False
         
