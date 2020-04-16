057f2c7086038d5700af7fe851d4a360b1679977
Tom Stephens <thomas.stephens@gmail.com>
2020-04-15 21:01:56 -0600

    Change window title

--- StarMapGen/src/SMGFrame.py
+++ StarMapGen/src/SMGFrame.py
@@ -2,5 +2,5 @@ import wx
 
 class SMGFrame(wx.Frame):    
     def __init__(self):
-        super().__init__(parent=None, title='Hello World')
+        super().__init__(parent=None, title='Star Map Generator')
         self.Show()
