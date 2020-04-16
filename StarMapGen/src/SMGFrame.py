bba3c1cc398445a6ae8e5e39028ca99b80c66e1a
Tom Stephens <thomas.stephens@gmail.com>
2020-04-15 21:17:17 -0600

    Add basic event to handle Generate Map button

--- StarMapGen/src/SMGFrame.py
+++ StarMapGen/src/SMGFrame.py
@@ -9,7 +9,15 @@ class SMGFrame(wx.Frame):
 		self.xSize = wx.TextCtrl(mainPanel)
 		inputSizer.Add(self.xSize,0,wx.ALL|wx.EXPAND,5)
 		generateBtn = wx.Button(mainPanel, label="Generate Map")
+		generateBtn.Bind(wx.EVT_BUTTON, self.generateMap)
 		inputSizer.Add(generateBtn,0,wx.ALL|wx.CENTER,5)
 		mainPanel.SetSizer(inputSizer)
 
 		self.Show()
+
+	def generateMap(self,event):
+		value = self.xSize.GetValue()
+		if not value:
+			print("You didn't enter anything")
+		else:
+			print(f'You typed: "{value}"')
