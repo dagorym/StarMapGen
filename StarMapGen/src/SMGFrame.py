b821ee9d3fd5e33b41f43fe2c92da2bfb40df3b1
Tom Stephens <thomas.stephens@gmail.com>
2020-04-15 21:07:19 -0600

    Add a few widgets

--- StarMapGen/src/SMGFrame.py
+++ StarMapGen/src/SMGFrame.py
@@ -1,6 +1,11 @@
 import wx
 
 class SMGFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='Star Map Generator')
-        self.Show()
+	def __init__(self):
+		super().__init__(parent=None, title='Star Map Generator')
+		mainPanel = wx.Panel(self)
+
+		self.xSize = wx.TextCtrl(mainPanel,pos=(5,5))
+		generateBtn = wx.Button(mainPanel, label="Generate Map",pos=(5,55))
+
+		self.Show()
